3a1bb0f4103e0033fd2eb8aa36ca13c7fadc1b0e
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2020-10-20 13:25:16 +0900

    Initial release for 1.1.0

---
+++ download.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+. $(dirname $0)/path.sh
+
+if [ -f tenes_$VERSION_BASE.orig.tar.gz ]; then
+  exit 127
+fi
+wget https://github.com/issp-center-dev/TeNeS/releases/download/v$VERSION_BASE/TeNeS-v$VERSION_BASE.tar.gz
+tar zxvf TeNeS-v$VERSION_BASE.tar.gz
+mv -f TeNeS-v$VERSION_BASE tenes_$VERSION_BASE
+tar zcvf tenes_$VERSION_BASE.orig.tar.gz tenes_$VERSION_BASE
+rm -rf tenes_$VERSION_BASE TeNeS-v$VERSION_BASE.tar.gz
