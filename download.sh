aeeb9f594d6e34c4499c1df56ee787998e0e4930
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2020-11-10 08:39:13 +0900

    New upstream version 1.1.2

--- download.sh
+++ download.sh
@@ -7,6 +7,5 @@ fi
 wget https://github.com/issp-center-dev/TeNeS/releases/download/v$VERSION_BASE/TeNeS-v$VERSION_BASE.tar.gz
 tar zxvf TeNeS-v$VERSION_BASE.tar.gz
 mv -f TeNeS-v$VERSION_BASE tenes_$VERSION_BASE
-find . -name '._*' | xargs rm -f
 tar zcvf tenes_$VERSION_BASE.orig.tar.gz tenes_$VERSION_BASE
 rm -rf tenes_$VERSION_BASE TeNeS-v$VERSION_BASE.tar.gz
