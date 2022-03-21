04fe9c53ad385b2e18cdd8c29a748e9c8f99b559
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2022-03-21 14:13:09 +0900

    New upstream version 1.2.0

--- download.sh
+++ download.sh
@@ -4,8 +4,8 @@
 if [ -f tenes_$VERSION_BASE.orig.tar.gz ]; then
   exit 127
 fi
-wget https://github.com/issp-center-dev/TeNeS/releases/download/v$VERSION_BASE/TeNeS-v$VERSION_BASE.tar.gz
-tar zxvf TeNeS-v$VERSION_BASE.tar.gz
-mv -f TeNeS-v$VERSION_BASE tenes_$VERSION_BASE
-tar zcvf tenes_$VERSION_BASE.orig.tar.gz tenes_$VERSION_BASE
-rm -rf tenes_$VERSION_BASE TeNeS-v$VERSION_BASE.tar.gz
+wget https://github.com/issp-center-dev/TeNeS/releases/download/v${VERSION_BASE}/TeNeS-${VERSION_BASE}.tar.gz
+mkdir tenes-${VERSION_BASE}
+tar zxvf TeNeS-${VERSION_BASE}.tar.gz -C tenes-${VERSION_BASE} --strip-component=1
+tar zcvf tenes_${VERSION_BASE}.orig.tar.gz tenes-${VERSION_BASE}
+rm -rf tenes-${VERSION_BASE} TeNeS-${VERSION_BASE}.tar.gz
